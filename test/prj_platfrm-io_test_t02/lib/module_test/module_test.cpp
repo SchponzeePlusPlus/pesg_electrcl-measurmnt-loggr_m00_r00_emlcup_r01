7ed074a8ae7dacee688135d6475e2cd78038363b
SchponzeePlusPlus <61894715+SchponzeePlusPlus@users.noreply.github.com>
2025-07-06 19:02:06 +1000

    #28 : PlatformIO Test T02 Project Updated - Replaced source code in project with source   code from PlatformIO Test T01 Project, then   moved module_test source code files from src/   to lib/ to test modularisation via PlatformIO,   but with more complex sub-directory   structuring and nesting than in PlatformIO Test   T00 Project, which had a flat structure

---
+++ test/prj_platfrm-io_test_t02/lib/module_test/module_test.cpp
@@ -0,0 +1,6 @@
+#include "module_test.h"
+
+// put function definitions here:
+int myFunction(int x, int y) {
+  return x + y;
+}
---
+++ test/prj_platfrm-io_test_t02/lib/module_test/module_test.h
@@ -0,0 +1,7 @@
+#ifndef INCLUDES_MODULE_TEST_H_
+#define INCLUDES_MODULE_TEST_H_
+
+// put function declarations here:
+int myFunction(int, int);
+
+#endif
